7d9baf6bee865ad0d4cb9c26bd2b0e4eb4025440
Jonatan Lazo <jonatan.lazo@lertait.com>
2020-11-20 12:15:44 -0600

    Remove obsolete imports and fix the path in file orm.config.json.sample

--- src/modules/user/dtos/user-create.dto.ts
+++ src/modules/user/dtos/user-create.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class UserCreateDto {
   @ApiProperty({ description: 'The user firstname', required: true })
