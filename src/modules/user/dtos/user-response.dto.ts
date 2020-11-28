0238a490bec0fc6c83db8de33c492db6151230bf
Jonatan Lazo <jonatan.lazo@lertait.com>
2020-11-27 21:04:18 -0600

    Update and disable users

--- src/modules/user/dtos/user-response.dto.ts
+++ src/modules/user/dtos/user-response.dto.ts
@@ -1,21 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
 export class UserResponseDto {
   @Expose()
+  @ApiProperty()
   id: number;
   @Expose()
+  @ApiProperty()
   firstname: string;
   @Expose()
+  @ApiProperty()
   lastname: string;
   @Expose()
+  @ApiProperty()
   email: string;
   @Expose()
+  @ApiProperty()
   isActive: boolean;
   @Expose()
-  authorId: number;
-  @Expose()
+  @ApiProperty()
   createdAt: Date;
   @Expose()
+  @ApiProperty()
   updatedAt: Date;
 }
---
+++ src/modules/user/dtos/user-update.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class UserUpdateDto {
+  @ApiProperty({ description: 'The user firstname', required: true })
+  @IsString({ message: 'user_firstname_required' })
+  firstname: string;
+  @ApiProperty({ description: 'The user lastname', required: true })
+  @IsString({ message: 'user_lastname_required' })
+  lastname: string;
+
+  authorId: number;
+}
