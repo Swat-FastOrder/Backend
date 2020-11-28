213955c64989db72f130dd037ac72bec7a2ee44a
Jonatan Lazo <jonatan.lazo@lertait.com>
2020-11-28 10:33:29 -0600

    Document the response order api

--- src/modules/order/dtos/order-response.dto.ts
+++ src/modules/order/dtos/order-response.dto.ts
@@ -1,24 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 import { OrderStatus } from '../order.status.enum';
 
 @Exclude()
 export class OrderResponseDto {
   @Expose()
+  @ApiProperty()
   id: number;
   @Expose()
+  @ApiProperty()
   waitressId: number;
   @Expose()
+  @ApiProperty()
   tableId: number;
   @Expose()
+  @ApiProperty()
   totalDiners: number;
   @Expose()
+  @ApiProperty()
   totalDishes: number;
   @Expose()
+  @ApiProperty()
   totalPrice: number;
   @Expose()
+  @ApiProperty()
   status: OrderStatus;
   @Expose()
+  @ApiProperty()
   createdAt: Date;
   @Expose()
+  @ApiProperty()
   updatedAt: Date;
 }
--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -9,8 +9,14 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiConflictResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiConflictResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { OrderCreateDto } from './dtos/order-create.dto';
+import { OrderResponseDto } from './dtos/order-response.dto';
 import { OrderService } from './order.service';
 
 @ApiTags('The orders')
@@ -19,6 +25,7 @@ export class OrderController {
   constructor(private readonly _orderService: OrderService) {}
 
   @ApiOperation({ summary: 'Retrieves the orders' })
+  @ApiOkResponse({ type: OrderResponseDto, isArray: true })
   @Get()
   @UseGuards(AuthGuard('jwt'))
   findAll() {
@@ -26,14 +33,15 @@ export class OrderController {
     return this._orderService.findAll({});
   }
 
-  @ApiOperation({ summary: 'Retrieves one menu category' })
+  @ApiOperation({ summary: 'Retrieves one order' })
+  @ApiOkResponse({ type: OrderResponseDto })
   @Get(':id')
   @UseGuards(AuthGuard('jwt'))
   findById(@Param('id') id: number) {
     return this._orderService.findById(id);
   }
 
-  @ApiOperation({ summary: 'Create menu category' })
+  @ApiOperation({ summary: 'Create order' })
   @ApiConflictResponse({
     description:
       'It happens when the table isnt available (order_table_not_ready_or_not_found)',
@@ -45,7 +53,7 @@ export class OrderController {
     return this._orderService.create(newOrder);
   }
 
-  @ApiOperation({ summary: 'Create menu category' })
+  @ApiOperation({ summary: 'Delete one order' })
   @Delete(':id')
   @UseGuards(AuthGuard('jwt'))
   delete(@Param('id') id: number) {
