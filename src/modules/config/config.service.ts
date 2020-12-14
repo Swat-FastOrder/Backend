0e8e215b858f93f889cedcf064ca8895fe776378
Jonatan Lazo <jonatan.lazo@lertait.com>
2020-12-14 15:02:36 -0600

    fix(ConfigService): Missing the URL_LOGIN variable

--- src/modules/config/config.service.ts
+++ src/modules/config/config.service.ts
@@ -24,6 +24,7 @@ export class ConfigService {
         JWT_SECRET: process.env.JWT_SECRET,
         MAIL_KEY: process.env.MAIL_KEY,
         BUSINESS_MAIL: process.env.BUSINESS_MAIL,
+        URL_LOGIN: process.env.URL_LOGIN,
         STORAGE_CLOUD_NAME: process.env.STORAGE_CLOUD_NAME,
         STORAGE_CLOUD_API_KEY: process.env.STORAGE_CLOUD_API_KEY,
         STORAGE_CLOUD_API_SECRET: process.env.STORAGE_CLOUD_API_SECRET,
